d8da252cd32fdff945139c288412ead7a40eba92
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-05-16 17:23:20 +0200

    Header css

--- script.js
+++ script.js
@@ -6,5 +6,6 @@ const header=document.querySelector("[data-header]");
 const navToggleBtn = document.querySelector("[data-menu-toggle-btn]")
 
 navToggleBtn.addEventListener("click", function(){
-    
+    header.classList.toggle("active");
+
 })
