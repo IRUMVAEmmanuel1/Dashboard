eb7899523b9b6bb44586b17b6390388465602f9a
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-05-16 17:03:57 +0200

    Header css

--- script.js
+++ script.js
@@ -2,4 +2,9 @@
 
 // toogle active class on header when cliecked nav-toogle-btn
 
-const header=document.querySelector("active")
+const header=document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-menu-toggle-btn]")
+
+navToggleBtn.addEventListener("click", function(){
+    
+})
