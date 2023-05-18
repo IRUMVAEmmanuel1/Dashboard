644fca0899a7e76e54c2c39dd82d3fd27bc9e67f
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-05-18 17:46:36 +0200

    toogle ctx-menu when click on card-menu-btn~ JS

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ navToggleBtn.addEventListener("click", function(){
     header.classList.toggle("active");
 
 })
+
+
+
+// toogle ctx-menu when click on card-menu-btn
+
+const menuBtn = document.querySelectorAll("[data-menu-btn]");
+
+for(let i=0;i<menuBtn.length; i++){
+    menuBtn[i].addEventListener("click", function(){
+        this.nextElementSibling.classList.toggle("active");
+    })
+}
